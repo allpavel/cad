14b8a37ef553bcbde808d183945668b46d1f6c64
Pavel Alloyarov <allpavel@yandex.ru>
2023-09-25 21:38:26 +0300

    add an overlay scrollbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { BiLogIn } from "react-icons/bi";
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
 import logo from "../../assets/images/logo.jpg";
 
 export const Header = () => {
@@ -7,24 +9,25 @@ export const Header = () => {
         <div className="p-3">
           <img src={logo} alt="" width={44} height={44} />
         </div>
-        <nav className="flex flex-row items-center overflow-x-auto overflow-y-hidden">
-          <a className="cursor-pointer px-6 py-3 font-semibold hover:bg-gray-50">
-            File
-          </a>
-          <a className="cursor-pointer px-6 py-3 font-semibold hover:bg-gray-50">
-            Home
-          </a>
-          <a className="cursor-pointer px-6 py-3 font-semibold hover:bg-gray-50">
-            View
-          </a>
-          <a className="cursor-pointer px-6 py-3 font-semibold hover:bg-gray-50">
-            Support
-          </a>
-        </nav>
-        <div>
-          <a className="cursor-pointer px-6 py-3 font-semibold hover:bg-gray-50">
-            Log in
-          </a>
+        <OverlayScrollbarsComponent defer>
+          <nav className="flex flex-row items-center">
+            <a className="cursor-pointer rounded-xl px-6 py-3 font-semibold hover:bg-gray-50">
+              File
+            </a>
+            <a className="cursor-pointer rounded-xl px-6 py-3 font-semibold hover:bg-gray-50">
+              Home
+            </a>
+            <a className="cursor-pointer rounded-xl px-6 py-3 font-semibold hover:bg-gray-50">
+              View
+            </a>
+            <a className="cursor-pointer rounded-xl px-6 py-3 font-semibold hover:bg-gray-50">
+              Support
+            </a>
+          </nav>
+        </OverlayScrollbarsComponent>
+        <div className="cursor-pointer rounded-xl px-6 py-3 hover:bg-gray-50">
+          <BiLogIn className=" text-xl md:hidden" />
+          <a className="hidden font-semibold md:block">Log in</a>
         </div>
       </div>
     </header>
